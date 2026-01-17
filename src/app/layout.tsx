da4b0ab9fe81f3dce1ee518d3be2fa70216776c7
Moritz Wecht <m.wecht@gmx.de>
2026-01-17 15:50:57 +0100

    change language to de

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="de" suppressHydrationWarning>
       <body className={`${roboto.variable} ${playfair.variable} antialiased`}>{children}</body>
     </html>
   );

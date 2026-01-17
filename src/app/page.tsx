c098aa3d866b1104bd40e89d2eaf19280cbf018f
Moritz Wecht <m.wecht@gmx.de>
2026-01-17 15:46:53 +0100

    add JSON LD for AI Bots

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -71,6 +71,46 @@ export default async function Home() {
       <Teaching />
 
       <Footer />
+
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            '@context': 'https://schema.org',
+            '@graph': [
+              {
+                '@type': 'Person',
+                '@id': 'https://christoph-blenker.de/#person',
+                name: 'Christoph Blenker',
+                jobTitle: ['Musician', 'Music Teacher'],
+                url: 'https://christoph-blenker.de',
+                image: 'https://christoph-blenker.de/icon.png',
+                sameAs: [
+                  // Add social profiles here if available globally, e.g. from a settings query
+                ]
+              },
+              ...events.map((event: any) => ({
+                '@type': 'Event',
+                name: event.title,
+                startDate: event.date,
+                eventStatus: 'https://schema.org/EventScheduled',
+                location: {
+                  '@type': 'Place',
+                  name: event.locationName,
+                  address: {
+                    '@type': 'PostalAddress',
+                    addressLocality: event.locationName // Fallback since we only have name
+                  }
+                },
+                offers: {
+                  '@type': 'Offer',
+                  url: event.locationUrl
+                }
+              }))
+            ]
+          })
+        }}
+      />
     </main>
   );
 }
